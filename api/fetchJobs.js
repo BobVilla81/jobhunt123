360625752870a9017f851e3b266902e5f71b5426
BobVilla81 <85167962+BobVilla81@users.noreply.github.com>
2024-02-14 12:54:57 +0000

    update for reed

--- api/fetchJobs.js
+++ api/fetchJobs.js
@@ -2,16 +2,15 @@ const axios = require('axios');
 
 const options = {
   method: 'GET',
-  url: 'https://www.reed.co.uk/api/1.0/search', // This is a hypothetical endpoint; check the actual Reed API docs
+  url: 'https://www.reed.co.uk/api/1.0/search',
   params: {
-    keywords: 'java',
-    location: 'austin, tx',
-    distanceFromLocation: '25',
-    // Add other parameters according to Reed's API documentation
+    keywords: 'accountant',
+    location: 'london',
+    employerid: 123,
+    distancefromlocation: 15
   },
   headers: {
-    // Authentication method as required by Reed, e.g., API Key in headers or query params
-    'Authorization': '5e8f0d41-22be-4a3f-9aee-034b9905b24b' 
+    'Authorization': 'Basic ' + Buffer.from('5e8f0d41-22be-4a3f-9aee-034b9905b24b').toString('base64')
   }
 };
 
